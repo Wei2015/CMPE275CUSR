f70382971f0708227b0e04f53659bcf8e3df1c3a
T G <taogeng3@gmail.com>
2017-11-22 19:21:23 -0800

    update home html

--- src/main/java/com/cmpe275/cusr/controller/HomeController.java
+++ src/main/java/com/cmpe275/cusr/controller/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class HelloController {
+public class HomeController {
 
 	// inject via application.properties
 	@Value("${welcome.message:test}")
@@ -16,6 +16,6 @@ public class HelloController {
 	@RequestMapping("/")
 	public String welcome(Map<String, Object> model) {
 		model.put("message", this.message);
-		return "hello";
+		return "home";
 	}
 }
