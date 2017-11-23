9411c1dd165e44cb96d07d075a90aa58a646f993
T G <taogeng3@gmail.com>
2017-11-22 17:06:38 -0800

    Springboot template

---
+++ src/main/java/com/cmpe275/cusr/CusrApplication.java
@@ -0,0 +1,12 @@
+package com.cmpe275.cusr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CusrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CusrApplication.class, args);
+	}
+}
