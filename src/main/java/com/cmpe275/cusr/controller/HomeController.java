83ae4390924e0e3437e2ade4cfee94162996597f
Wei Yao <wei.yao@sjsu.edu>
2017-11-23 00:24:42 -0800

    modify home.html to add search options

--- src/main/java/com/cmpe275/cusr/controller/HomeController.java
+++ src/main/java/com/cmpe275/cusr/controller/HomeController.java
@@ -4,11 +4,12 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-
+	/*
 	// inject via application.properties
 	@Value("${welcome.message:test}")
 	private String message = "Hello World";
@@ -16,6 +17,13 @@ public class HomeController {
 	@RequestMapping("/")
 	public String welcome(Map<String, Object> model) {
 		model.put("message", this.message);
+		return "home";
+	}
+	*/
+	
+	@RequestMapping("/")
+	public String index(Model model) {
+		
 		return "home";
 	}
 }
