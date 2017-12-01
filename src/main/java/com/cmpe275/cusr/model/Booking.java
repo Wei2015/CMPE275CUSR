01518067d7a05a6e550760d06b21509788cd9685
T G <taogeng3@gmail.com>
2017-12-01 12:11:51 -0800

    update booking

--- src/main/java/com/cmpe275/cusr/model/Booking.java
+++ src/main/java/com/cmpe275/cusr/model/Booking.java
@@ -4,56 +4,24 @@ import java.util.List;
 
 public class Booking {
 	
-	private java.util.Calendar departDate;
-	private List<Long> trainId;
-	private List<Station> station;
-	private List<java.util.Date> departTime;
-	private Station arrivalStation;
-	private java.util.Date arrivalTime;
+	private java.util.Calendar departureDate;
+	private List<Segment> departureTrip;
 	private java.util.Calendar returnDate;
-	private List<Long> returnTrainId;
-	private List<Station> returnStation;
-	private List<java.util.Date> returnDepartTime;
-	private Station returnArrivalStation;
-	private java.util.Date returnArrivalTime;
+	private List<Segment> returnTrip;
 	private int numOfSeats;
 	private double price;
 	
-	public java.util.Calendar getDepartDate() {
-		return departDate;
+	public java.util.Calendar getDepartureDate() {
+		return departureDate;
 	}
-	public void setDepartDate(java.util.Calendar departDate) {
-		this.departDate = departDate;
+	public void setDepartureDate(java.util.Calendar departureDate) {
+		this.departureDate = departureDate;
 	}
-	public List<Long> getTrainId() {
-		return trainId;
+	public List<Segment> getDepartureTrip() {
+		return departureTrip;
 	}
-	public void setTrainId(List<Long> trainId) {
-		this.trainId = trainId;
-	}
-	public List<Station> getStation() {
-		return station;
-	}
-	public void setStation(List<Station> station) {
-		this.station = station;
-	}
-	public List<java.util.Date> getDepartTime() {
-		return departTime;
-	}
-	public void setDepartTime(List<java.util.Date> departTime) {
-		this.departTime = departTime;
-	}
-	public Station getArrivalStation() {
-		return arrivalStation;
-	}
-	public void setArrivalStation(Station arrivalStation) {
-		this.arrivalStation = arrivalStation;
-	}
-	public java.util.Date getArrivalTime() {
-		return arrivalTime;
-	}
-	public void setArrivalTime(java.util.Date arrivalTime) {
-		this.arrivalTime = arrivalTime;
+	public void setDepartureTrip(List<Segment> departureTrip) {
+		this.departureTrip = departureTrip;
 	}
 	public java.util.Calendar getReturnDate() {
 		return returnDate;
@@ -61,35 +29,11 @@ public class Booking {
 	public void setReturnDate(java.util.Calendar returnDate) {
 		this.returnDate = returnDate;
 	}
-	public List<Long> getReturnTrainId() {
-		return returnTrainId;
+	public List<Segment> getReturnTrip() {
+		return returnTrip;
 	}
-	public void setReturnTrainId(List<Long> returnTrainId) {
-		this.returnTrainId = returnTrainId;
-	}
-	public List<Station> getReturnStation() {
-		return returnStation;
-	}
-	public void setReturnStation(List<Station> returnStation) {
-		this.returnStation = returnStation;
-	}
-	public List<java.util.Date> getReturnDepartTime() {
-		return returnDepartTime;
-	}
-	public void setReturnDepartTime(List<java.util.Date> returnDepartTime) {
-		this.returnDepartTime = returnDepartTime;
-	}
-	public Station getReturnArrivalStation() {
-		return returnArrivalStation;
-	}
-	public void setReturnArrivalStation(Station returnArrivalStation) {
-		this.returnArrivalStation = returnArrivalStation;
-	}
-	public java.util.Date getReturnArrivalTime() {
-		return returnArrivalTime;
-	}
-	public void setReturnArrivalTime(java.util.Date returnArrivalTime) {
-		this.returnArrivalTime = returnArrivalTime;
+	public void setReturnTrip(List<Segment> returnTrip) {
+		this.returnTrip = returnTrip;
 	}
 	public int getNumOfSeats() {
 		return numOfSeats;
@@ -102,5 +46,5 @@ public class Booking {
 	}
 	public void setPrice(double price) {
 		this.price = price;
-	}	
+	}
 }
