f33e286d4bf7790f11f396f84245acce6b3004fe
Judy Yang <judy0821.yang@gmail.com>
2017-12-14 21:01:21 -0800

    disable csrf for posts to api endpoints

--- src/main/java/com/cmpe275/cusr/config/SecurityConfig.java
+++ src/main/java/com/cmpe275/cusr/config/SecurityConfig.java
@@ -19,6 +19,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
 			.antMatchers("/user/**").hasRole("USER")
-			.anyRequest().permitAll();
+			.anyRequest().permitAll()
+			.and().csrf().disable();
 	}
 }
