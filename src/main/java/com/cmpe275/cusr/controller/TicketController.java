a8d5f37f47d75eadd53d53c4950804daed2fd855
T G <taogeng3@gmail.com>
2017-12-14 21:55:24 -0800

    update ticket controller

--- src/main/java/com/cmpe275/cusr/controller/TicketController.java
+++ src/main/java/com/cmpe275/cusr/controller/TicketController.java
@@ -1,7 +1,6 @@
 package com.cmpe275.cusr.controller;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +10,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.cmpe275.cusr.model.Booking;
+import com.cmpe275.cusr.model.Segment;
 import com.cmpe275.cusr.model.User;
 import com.cmpe275.cusr.service.TicketService;
 import com.cmpe275.cusr.service.UserService;
@@ -23,6 +23,33 @@ public class TicketController {
 
 	@Autowired
 	TicketService ticketService;
+	
+	private List<Segment> departureTrip = new ArrayList<Segment>();
+	public TicketController() {
+		departureTrip.add(new Segment(S, ))
+	}
+	
+	@PostMapping("/user/select")
+	public String select(Model model, @ModelAttribute Booking booking) { 
+		String returnDate = booking.getReturnDate();
+		int numOfSeats = booking.getNumOfSeats();
+		List<String> passenger = new ArrayList<>();
+		for (int i = 0; i < numOfSeats; ++i) {
+			passenger.add("");
+		}
+		model.addAttribute("passenger", passenger);
+		model.addAttribute("numOfSeats", numOfSeats);
+		model.addAttribute("price", booking.getPrice());
+		model.addAttribute("totalPrice", booking.getPrice() + 1);
+		model.addAttribute("departureDate", booking.getDepartureDate());
+		model.addAttribute("departureTrip", booking.getDepartureTrip());
+		model.addAttribute("round", returnDate == null ? "Y" : "N");
+		if (returnDate != null) {
+			model.addAttribute("returnDate", returnDate);
+			model.addAttribute("returnTrip", booking.getReturnTrip());
+		}
+		return "select";
+	}
 
 	@PostMapping("/user/purchase")
 	public String purchase(Model model, @ModelAttribute Booking booking) {
@@ -35,30 +62,32 @@ public class TicketController {
 		 * model.addAttribute("times", times); return "purchase"; }
 		 */
 		/*User user = userService.findUser();
+		model.addAttribute("email", user.getEmail());
 		if (ticketService.purchase(user, booking)) {
-			Date returnDate = booking.getReturnDate();
+			String returnDate = booking.getReturnDate();
 			int numOfSeats = booking.getNumOfSeats();
-			model.addAttribute("name", user.getName());
-			model.addAttribute("email", user.getEmail());
+			model.addAttribute("passenger", booking.getPassenger());
 			model.addAttribute("numOfSeats", numOfSeats);
-			model.addAttribute("price", booking.getPrice());
+			model.addAttribute("numOfTickets", numOfSeats > 1 ? "multiple" : "single");
+			model.addAttribute("totalPrice", booking.getPrice() + 1);
 			model.addAttribute("departureDate", booking.getDepartureDate());
 			model.addAttribute("departureTrip", booking.getDepartureTrip());
-			model.addAttribute("returnDate", returnDate);
-			model.addAttribute("returnTrip", booking.getReturnTrip());
 			model.addAttribute("round", returnDate == null ? "Y" : "N");
-			model.addAttribute("numOfTickets", numOfSeats > 1 ? "multiple" : "single");
+			if (returnDate != null) {
+				model.addAttribute("returnDate", returnDate);
+				model.addAttribute("returnTrip", booking.getReturnTrip());
+			}
 			return "purchase_success";
 		} else {
 			return "purchase_fail";
 		}
 	}
 
-	@PostMapping("/user/ticketCancel")
+	/*@PostMapping("/user/ticketCancel")
 	public String cancel(Model model, @ModelAttribute Ticket ticket) {
 		User user = userService.findUser();
 		if (ticketService.cancel(user, booking)) {
-			Date returnDate = booking.getReturnDate();
+			String returnDate = booking.getReturnDate();
 			int numOfSeats = booking.getNumOfSeats();
 			model.addAttribute("name", user.getName());
 			model.addAttribute("email", user.getEmail());
