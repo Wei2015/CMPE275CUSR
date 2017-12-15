4958b433f1aa56f614287ef0b903ad43ce1ecf66
T G <taogeng3@gmail.com>
2017-12-14 19:47:07 -0800

    update Email Service

--- src/main/java/com/cmpe275/cusr/service/EmailService.java
+++ src/main/java/com/cmpe275/cusr/service/EmailService.java
@@ -1,5 +1,13 @@
 package com.cmpe275.cusr.service;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 public interface EmailService {
-	public void sendMail(String toEmail, String subject, String text);
+
+	public void sendMail(String toEmail, String subject, String message) throws Exception;
+
+	public String getURLSource(String url) throws IOException;
+
+	public String toString(InputStream inputStream) throws IOException;
 }
--- src/main/java/com/cmpe275/cusr/service/EmailServiceImpl.java
+++ src/main/java/com/cmpe275/cusr/service/EmailServiceImpl.java
@@ -1,23 +1,61 @@
 package com.cmpe275.cusr.service;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 @Service
-public class EmailServiceImpl implements EmailService{
-	
+public class EmailServiceImpl implements EmailService {
+
 	@Autowired
 	private JavaMailSender javaMailSender;
-
-    public void sendMail(String toEmail, String subject, String text) {
+	
+	//Send text email.
+    /*public void sendMail(String toEmail, String subject, String text) {
         SimpleMailMessage mailMessage = new SimpleMailMessage();
         mailMessage.setTo(toEmail);
         mailMessage.setSubject(subject);
         mailMessage.setText(text);
         javaMailSender.send(mailMessage);
-    }
+    }*/
+
+	// Send HTML email.
+	public void sendMail(String toEmail, String subject, String message) throws Exception {
+		MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+		MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, false, "utf-8");
+		String htmlMsg = message;
+		mimeMessage.setContent(htmlMsg, "text/html");
+		helper.setTo(toEmail);
+		helper.setSubject(subject);
+		javaMailSender.send(mimeMessage);
+	}
+
+	public String getURLSource(String url) throws IOException {
+		URL urlObject = new URL(url);
+		URLConnection urlConnection = urlObject.openConnection();
+		urlConnection.setRequestProperty("User-Agent",
+				"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
+		return toString(urlConnection.getInputStream());
+	}
+
+	public String toString(InputStream inputStream) throws IOException {
+		try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"))) {
+			String inputLine;
+			StringBuilder stringBuilder = new StringBuilder();
+			while ((inputLine = bufferedReader.readLine()) != null) {
+				stringBuilder.append(inputLine);
+			}
+			return stringBuilder.toString();
+		}
+	}
 }
-
-
