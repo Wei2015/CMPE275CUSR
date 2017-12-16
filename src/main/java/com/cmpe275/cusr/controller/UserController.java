96a1438df5d3337c6bd234762d7044b3d08264fd
Wei Yao <wei.yao@sjsu.edu>
2017-12-15 17:02:09 -0800

    connect train selection to purchase

--- src/main/java/com/cmpe275/cusr/controller/UserController.java
+++ src/main/java/com/cmpe275/cusr/controller/UserController.java
@@ -1,30 +1,27 @@
 package com.cmpe275.cusr.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.security.access.annotation.Secured;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Service;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.cmpe275.cusr.model.OneWayList;
+import com.cmpe275.cusr.model.OneWayTrip;
 import com.cmpe275.cusr.model.SearchContent;
 import com.cmpe275.cusr.service.TrainService;
 import com.cmpe275.cusr.service.UserService;
 
 
 @Controller
-@SessionAttributes(value={"oneWayList", "searchContent"})
+@SessionAttributes(value={"oneWayList", "searchContent", "oneWayTrip", "returnWayList"})
 public class UserController {
 	
 	@Autowired
@@ -66,7 +63,8 @@ public class UserController {
 								@ModelAttribute("searchContent") SearchContent search,
 								@RequestParam("Select") String selectTrip, Model model) {
 		int tripIndexSelected = Integer.valueOf(selectTrip.substring(4, 5))-1;
-		model.addAttribute(oneWayList.getFirstFive().get(tripIndexSelected));
+		OneWayTrip forwardTrip = oneWayList.getFirstFive().get(tripIndexSelected);
+		model.addAttribute("oneWayTrip", forwardTrip);
 		
 		
 		OneWayList returnResult = trainService.searchOneWay(search.getReturnSearch());
@@ -74,5 +72,40 @@ public class UserController {
 		
 		return "selectReturn";
 	}
+	
+	@PostMapping("/purchase")
+	public String select(@ModelAttribute("oneWayTrip") OneWayTrip forwardTrip, 
+						@ModelAttribute("searchContent") SearchContent search,
+						@ModelAttribute("returnWayList") OneWayList returnWayList,
+						@RequestParam("Select") String selectTrip, Model model) { 
+		
+		//obtain return trip object
+		int tripIndexSelected = Integer.valueOf(selectTrip.substring(4, 5))-1;
+		OneWayTrip returnTrip = returnWayList.getFirstFive().get(tripIndexSelected);
+		
+		//fill in model
+		int numOfSeats = search.getNumberOfSeats();
+		model.addAttribute("numOfSeats", numOfSeats);
+		List<String> passenger = new ArrayList<>();
+		for (int i = 0; i < numOfSeats; ++i) {
+			passenger.add("");
+		}
+		model.addAttribute("passenger", passenger);
+		
+		double price= forwardTrip.getTicketPrice() + returnTrip.getTicketPrice();
+		model.addAttribute("price", price);
+		model.addAttribute("totalPrice", price + 1);
+		model.addAttribute("departureDate", search.getDepartureDate());
+		
+		model.addAttribute("departureTrip", forwardTrip.getConnections());
+		String returnDate = search.getReturnDate();
+		model.addAttribute("round", search.isRoundTrip()? "Y" : "N");
+		if (returnDate != null) {
+			model.addAttribute("returnDate", returnDate);
+			model.addAttribute("returnTrip", returnTrip.getConnections());
+		}
+		return "purchase";
+	}
+	
 	
 }
