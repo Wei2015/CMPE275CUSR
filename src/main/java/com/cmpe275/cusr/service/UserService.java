9a91543870c370026a8ea8cf249bd5d0e9613c57
Judy Yang <judy0821.yang@gmail.com>
2017-12-20 00:01:09 -0800

    Log out functionality

--- src/main/java/com/cmpe275/cusr/service/UserService.java
+++ src/main/java/com/cmpe275/cusr/service/UserService.java
@@ -5,5 +5,6 @@ import com.cmpe275.cusr.model.User;
 public interface UserService {
 	public User findUser();
 	public void signInAuthentication(String firebaseToken);
+	public void signout();
 	public User getUserFromDB(String email);
 }
